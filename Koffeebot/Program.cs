af348e7e9d22fae18c6442f44835eda3628bd49b
Zlata <zlatakravcenko7@gmail.com>
2026-05-20 22:30:53 +0300

    добавлены изменения

--- Koffeebot/Program.cs
+++ Koffeebot/Program.cs
@@ -3,12 +3,18 @@ using Koffeebot.Repositories.Implementations;
 using Koffeebot.Repositories.Interfaces;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
+using System.Net;
 using Telegram.Bot;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var telegramToken = "8730936001:AAGP4_YBMftEOPNzia7P5PoSh8sVev0lCS0";
 builder.Services.AddSingleton<ITelegramBotClient>(sp => new TelegramBotClient(telegramToken));
+//var proxy = new WebProxy("http://proxy.minicas.best:443");
+//var handler = new HttpClientHandler { Proxy = proxy };
+//var client = new HttpClient(handler);
+//builder.Services.AddSingleton<ITelegramBotClient>(sp => new TelegramBotClient(telegramToken, client));
+
 builder.Services.AddSingleton<IBotCommand, StartCommand>();
 builder.Services.AddSingleton<IBotCommand, HelpCommand>();
 builder.Services.AddSingleton<TelegramUpdateProcessor>();
